29d7420f5d993bde31cdaf6baf88530963267006
redpheonixxx <30800592+redpheonixxx@users.noreply.github.com>
2017-08-10 13:24:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/aboutmyself', funtion(req,res) {
+app.get('/abt', function(req, res) {
     res.send('King is always king');
 });
 
